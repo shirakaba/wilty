020f4ad1a4ebd32dd22ab467b14fcaaff70892e8
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2020-09-25 16:51:16 +0100

    Convert to Svelte

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var port = process.env.PORT || 3000,
     http = require('http'),
     fs = require('fs'),
-    html = fs.readFileSync('index.html');
+    html = fs.readFileSync('public/index.html');
 
 var log = function(entry) {
     fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
